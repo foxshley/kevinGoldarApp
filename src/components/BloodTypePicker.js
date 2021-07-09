5c3d32365fd5f2d25124464c50febde11907915f
Andriana Hadi <andriana.hadisurya@gmail.com>
2021-07-09 11:49:55 +0700

    Fix the value of A- blood type

--- src/components/BloodTypePicker.js
+++ src/components/BloodTypePicker.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { StyleSheet } from 'react-native'
-import { Picker } from '@react-native-picker/picker';
-
+import {StyleSheet} from 'react-native';
+import {Picker} from '@react-native-picker/picker';
 
 const styles = StyleSheet.create({
   input: {
@@ -9,22 +8,22 @@ const styles = StyleSheet.create({
     marginHorizontal: 'auto',
     marginBottom: 10,
     paddingHorizontal: 20,
-    borderRadius: 10
-  }
+    borderRadius: 10,
+  },
 });
 
-const BloodTypePicker = (props) => (
-    <Picker {...props} style={styles.input} backgroundColor="#ECE6E6">
-      <Picker.Item label="-- Pilih Golongan Darah --" value="" />
-      <Picker.Item label="A+" value="A+" />
-      <Picker.Item label="A-" value="A+" />
-      <Picker.Item label="B+" value="B+" />
-      <Picker.Item label="B-" value="B-" />
-      <Picker.Item label="AB+" value="AB+" />
-      <Picker.Item label="AB-" value="AB-" />
-      <Picker.Item label="O+" value="O+" />
-      <Picker.Item label="O-" value="O-" />
-    </Picker>
+const BloodTypePicker = props => (
+  <Picker {...props} style={styles.input} backgroundColor="#ECE6E6">
+    <Picker.Item label="-- Pilih Golongan Darah --" value="" />
+    <Picker.Item label="A+" value="A+" />
+    <Picker.Item label="A-" value="A-" />
+    <Picker.Item label="B+" value="B+" />
+    <Picker.Item label="B-" value="B-" />
+    <Picker.Item label="AB+" value="AB+" />
+    <Picker.Item label="AB-" value="AB-" />
+    <Picker.Item label="O+" value="O+" />
+    <Picker.Item label="O-" value="O-" />
+  </Picker>
 );
 
 export default BloodTypePicker;
