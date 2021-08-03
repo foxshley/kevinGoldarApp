56ff89feb5d309225ade31477dff88f0399761e5
Andriana Hadi <andriana.hadisurya@gmail.com>
2021-08-03 13:39:28 +0700

    Reworked slides caption

--- src/slides.js
+++ src/slides.js
@@ -3,19 +3,20 @@ export default [
     id: '1',
     title: 'We need you',
     description:
-      'Ex ullamco nisi enim Lorem excepteur dolor aute do consectetur amet.',
+      'Jutaan orang dalam kondisi kritis dan membutuhkan darah, andalah satu-satunya harapan mereka.',
     image: require('./assets/slides/1.jpg'),
   },
   {
     id: '2',
     title: 'What you can do',
-    description: 'Ea dolore do irure aute veniam sunt sit.',
+    description:
+      'Uluran tangan anda menentukan nasib mereka, anda dapat mendonorkan darah untuk meringankan beban mereka.',
     image: require('./assets/slides/2.jpg'),
   },
   {
     id: '3',
     title: 'Take action',
-    description: 'Irure irure laborum in qui.',
+    description: 'Tekan Start untuk bersama mengikuti gerakan donor darah.',
     image: require('./assets/slides/3.jpg'),
   },
 ];
